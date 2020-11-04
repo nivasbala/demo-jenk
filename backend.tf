154bc5923b284ad2a126cacc76a35313db23aca7
Srini Bala <37417818+nivasbala@users.noreply.github.com>
2020-11-04 00:15:13 -0800

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "http" {
-    address = "https://objectstorage.us-phoenix-1.oraclecloud.com/p/ABBRiEhRajOpoc7WwKeUwwnRaxRpcsLvQXmibFbGkPnITJ_N0wJm5kMNVAkKDE8u/n/orasenatdoracledigital01/b/offdepdemo/o/tfstateterraform.tfstate"
+    address = "https://objectstorage.us-phoenix-1.oraclecloud.com/p/6m6IpyBseO7SDh4QvfBjmjuusEPeKKNHJf2MdJZi45cBHhz8kCZLAkcINSG7v1ds/n/orasenatdoracledigital01/b/offdepdemo/o/tfstateterraform.tfstate"
     
     update_method = "PUT"
   }
