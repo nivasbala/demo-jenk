d998056d49abed7504edf7ad8d70577430cab030
Oracle Public Cloud User <opc@cicd-node-383068.sub10082340500.offdepjenkins.oraclevcn.com>
2020-11-04 00:23:47 +0000

    Warning removed

--- create-vcn.tf
+++ create-vcn.tf
@@ -1,7 +1,7 @@
 resource "oci_core_vcn" "testone_jenk_vcn" {
     #Required
     cidr_block = "10.0.0.0/16"
-    compartment_id = "${var.compartment_ocid}"
+    compartment_id = var.compartment_ocid
 
     #Optional
     display_name = "tf-from-jenk-vcn"
