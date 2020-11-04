0350979fda8af06157aa5035ef502ebb8260e4c1
Srini Bala <37417818+nivasbala@users.noreply.github.com>
2020-11-03 16:20:54 -0800

    Update create-vcn.tf

--- create-vcn.tf
+++ create-vcn.tf
@@ -1,8 +1,7 @@
 resource "oci_core_vcn" "testone_jenk_vcn" {
     #Required
     cidr_block = "10.0.0.0/16"
-    #compartment_id = "${var.compartment_ocid}"
-    compartment_id = "ocid1.compartment.oc1..aaaaaaaalhkxjm3hszmngwgrhko6g2d6tgrqtnqpmaijnmkkor7alimuyqsq"
+    compartment_id = "${var.compartment_ocid}"
 
     #Optional
     display_name = "tf-from-jenk-vcn"
